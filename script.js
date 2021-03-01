5df8daec4d1350f06a79e47eb2b5343ac0e896b1
konartur <suntikflood@gmail.com>
2021-03-01 22:07:57 +0200

    created counter

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('final test');
+let btnMinus = document.querySelector('.btn_minus');
+let btnPlus = document.querySelector('.btn_plus');
+let result = document.querySelector('.result');
+let count = 0;
+
+btnMinus.addEventListener('click', () => {
+    count--;
+    result.innerHTML = count;
+});
+
+btnPlus.addEventListener('click', () => {
+    count++;
+    result.innerHTML = count;
+});
