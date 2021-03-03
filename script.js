de436a41116f45eed01a50800562a6eb64f1153b
konartur <suntikflood@gmail.com>
2021-03-03 17:05:22 +0200

    edited to class

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let btnMinus = document.querySelector('.btn_minus');
 let btnPlus = document.querySelector('.btn_plus');
 let result = document.querySelector('.result');
-let count = 0;
+class Count {
+    constructor(count) {
+        this.count = count;
+    }
 
-btnMinus.addEventListener('click', () => {
-    count--;
-    result.innerHTML = count;
-});
+    minus() {
+        this.count--;
+        result.innerHTML = this.count;
+    }
 
-btnPlus.addEventListener('click', () => {
-    count++;
-    result.innerHTML = count;
-});
+    plus() {
+        this.count++;
+        result.innerHTML = this.count;
+    }
+
+
+}
+
+let calc = new Count(0);
+
+btnMinus.addEventListener('click', calc.minus());
+btnPlus.addEventListener('click', calc.plus());
