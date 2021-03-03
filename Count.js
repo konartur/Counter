d88a2233ec0123ff4498ba18ad6578ba6577fd20
konartur <suntikflood@gmail.com>
2021-03-03 17:23:47 +0200

    created file for class

---
+++ Count.js
@@ -0,0 +1,14 @@
+export default class Count {
+    constructor(count) {
+        this.count = count;
+    }
+
+    minus() {
+        this.count--;
+    }
+
+    plus() {
+        this.count++;
+    }
+}
+
--- script.js
+++ script.js
@@ -1,25 +1,17 @@
+import Count from './Count';
+
 let btnMinus = document.querySelector('.btn_minus');
 let btnPlus = document.querySelector('.btn_plus');
 let result = document.querySelector('.result');
-class Count {
-    constructor(count) {
-        this.count = count;
-    }
-
-    minus() {
-        this.count--;
-        result.innerHTML = this.count;
-    }
-
-    plus() {
-        this.count++;
-        result.innerHTML = this.count;
-    }
-
-
-}
 
 let calc = new Count(0);
 
-btnMinus.addEventListener('click', calc.minus());
-btnPlus.addEventListener('click', calc.plus());
+btnMinus.addEventListener('click', () => {
+    calc.minus();
+    result.innerHTML = this.count;
+});
+
+btnPlus.addEventListener('click', () => {
+    calc.plus();
+    result.innerHTML = this.count;
+});
